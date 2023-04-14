81ad795c6805822297a30a94a753e81ed8e1f8d6
Historia Montague <historia.montague@outlook.com>
2023-04-14 13:03:29 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(username, userEmail) {
+    const body = document.querySelector("body");
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail,
+        })
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+        body.innerHTML = `<p>${obj.id}<p>`
+    })
+    .catch(error =>  {
+        body.innerHTML = `<p>${error.message}<p>`
+    });
+};
